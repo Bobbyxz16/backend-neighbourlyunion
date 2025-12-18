56260ddb23086866923d77c89592bbea6c52af3b
Bobbyxz16 <bobbyokolie14@gmail.com>
2025-12-18 11:08:08 +0000

    New version

--- src/main/java/com/example/neighborhelp/config/FirebaseInitializer.java
+++ src/main/java/com/example/neighborhelp/config/FirebaseInitializer.java
@@ -1,8 +1,6 @@
 package com.example.neighborhelp.config;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
+
 import jakarta.annotation.PostConstruct;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
@@ -13,23 +11,5 @@ import java.io.InputStream;
 @Component
 public class FirebaseInitializer {
 
-    @PostConstruct
-    public void initialize() {
-        try {
-            InputStream serviceAccount = new ClassPathResource("firebase-config.json").getInputStream();
 
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setProjectId("neighborhelp-e7f2b")
-                    .build();
-
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-                System.out.println("Firebase Admin SDK initialized");
-            }
-        } catch (IOException e) {
-            System.err.println("Error initializing Firebase Admin SDK: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
 }
